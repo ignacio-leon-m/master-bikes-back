c7b48745c4a0091f718fcdd77bff84eb4b6bf3b5
ignacio-leon-m <ig.leon@duocuc.cl>
2023-07-08 01:01:35 -0400

    listado de arriendo ok

--- registro-usuarios-spring-security/src/main/java/com/registro/usuarios/servicio/ArriendoServicio.java
+++ registro-usuarios-spring-security/src/main/java/com/registro/usuarios/servicio/ArriendoServicio.java
@@ -3,6 +3,8 @@ package com.registro.usuarios.servicio;
 import com.registro.usuarios.dto.ArriendoDTO;
 import com.registro.usuarios.modelo.Arriendo;
 
+import java.util.List;
+
 /**
  * Created by ignac on 23-06-2023.
  */
@@ -14,5 +16,4 @@ public interface ArriendoServicio {
     void procesarPago(Arriendo arriendo, String formaPago, String detallePago);
 
     void solicitarGarantia(Arriendo arriendo);
-
 }
